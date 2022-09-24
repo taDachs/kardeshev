b9f964b47c75261fead4b745fd2edfbb42c11300
Max Schik <max.schik@googlemail.com>
2022-09-25 01:22:31 +0200

    removes old terminal visualizer

--- src/main.cpp
+++ src/main.cpp
@@ -27,51 +27,6 @@ const std::vector<kardeshev::PlanetClass> PLANETS{kardeshev::DESERT,
                                             kardeshev::TERRESTIAL};
 const std::vector<kardeshev::StarClass> STARS{kardeshev::O, kardeshev::B, kardeshev::G};
 
-/**/
-/* int main_old() */
-/* { */
-/*   auto pg = std::make_shared<kardeshev::NaivePlanetGenerator>(PLANETS); */
-/*   auto sg = std::make_shared<kardeshev::NaiveStarGenerator>(STARS); */
-/*   kardeshev::NaiveSolarSystemGenerator ssg(pg, sg, 3, 10); */
-/**/
-/*   std::map<std::string, std::string> visualizer; */
-/*   visualizer[kardeshev::DESERT.getKey()]     = kardeshev::ASCII_MARS; */
-/*   visualizer[kardeshev::GAS_DWARF.getKey()]  = kardeshev::ASCII_VENUS; */
-/*   visualizer[kardeshev::GAS_GIANT.getKey()]  = kardeshev::ASCII_JUPITER; */
-/*   visualizer[kardeshev::HYCEAN.getKey()]     = kardeshev::ASCII_EARTH; */
-/*   visualizer[kardeshev::ICE_GIANT.getKey()]  = kardeshev::ASCII_PLUTO; */
-/*   visualizer[kardeshev::ICE.getKey()]        = kardeshev::ASCII_PLUTO; */
-/*   visualizer[kardeshev::IRON.getKey()]       = kardeshev::ASCII_MERKUR; */
-/*   visualizer[kardeshev::LAVA.getKey()]       = kardeshev::ASCII_VENUS; */
-/*   visualizer[kardeshev::OCEAN.getKey()]      = kardeshev::ASCII_EARTH; */
-/*   visualizer[kardeshev::PROTO.getKey()]      = kardeshev::ASCII_VENUS; */
-/*   visualizer[kardeshev::TERRESTIAL.getKey()] = kardeshev::ASCII_MERKUR; */
-/*   visualizer[kardeshev::O.getKey()]          = kardeshev::ASCII_O_STAR; */
-/*   visualizer[kardeshev::B.getKey()]          = kardeshev::ASCII_B_STAR; */
-/*   visualizer[kardeshev::G.getKey()]          = kardeshev::ASCII_SUN; */
-/**/
-/*   kardeshev::ResourceType potatoes("potato_resource", "Potatoes"); */
-/*   kardeshev::Need food_req(potatoes, 1); */
-/*   std::shared_ptr<kardeshev::Building> potato_farm = std::make_shared<kardeshev::Farm>(potatoes); */
-/*   kardeshev::Species humans("human_species", "Humans", 1.4, {food_req}); */
-/*   kardeshev::Population pop(humans, {}); */
-/**/
-/*   kardeshev::TerminalVisualizer tv(visualizer); */
-/**/
-/**/
-/*   std::shared_ptr<kardeshev::Planet> planet = pg->generatePlanet(); */
-/*   planet->addBuilding(potato_farm); */
-/*   planet->addPop(pop); */
-/*   while (true) */
-/*   { */
-/*     std::cout << tv.drawPlanet(*planet) << std::endl; */
-/*     sleep(1); */
-/*     planet->update(); */
-/*   } */
-/**/
-/*   return 0; */
-/* } */
-/**/
 int main()
 {
   auto game = std::make_shared<kardeshev::Game>();
